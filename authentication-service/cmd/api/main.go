9644236882de05e36ebcc233e0ac451d4adb81ed
DaNiEl SoGbEy <sogbeydaniel28@gmail.com>
2025-04-14 07:58:34 +0000

    added a routes method and progres connection method

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"authentication/data"
+	"context"
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -13,5 +21,65 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting authentication service")
+
+	// connect to DB
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Fatal("Can't connect to postgres!")
+	}
+
+	//set up config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.PingContext(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("AUTH_MICRO_DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 
 }
