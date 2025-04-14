c555bd9475f23ffe3bc530f9944273fd8732ac94
DaNiEl SoGbEy <sogbeydaniel28@gmail.com>
2025-04-14 01:04:11 +0000

    using the write json helper method

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker service",
 	}
 
-	responseBytes, err := json.Marshal(payload)
-
-	if err != nil {
-
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(responseBytes)
-	if err != nil {
-
-	}
+	_ = app.writeJson(w, http.StatusOK, payload)
 
 }
