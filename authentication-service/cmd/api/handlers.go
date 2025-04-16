7e6af6e43c7ab0881868d1626ea991e4d1bdb0bd
DaNiEl SoGbEy <sogbeydaniel28@gmail.com>
2025-04-16 00:11:56 +0000

    using the logger-service in the authentication service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/tsawler/toolbox"
@@ -33,6 +35,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// log authentication
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		_ = tools.ErrorJSON(w, err)
+		return
+	}
+
 	payload := toolbox.JSONResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in %s", user.Email),
@@ -40,4 +49,28 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = tools.WriteJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.Marshal(&entry)
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	_, err = client.Do(request)
+
+	return err
 }
